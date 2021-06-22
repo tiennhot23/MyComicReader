7f0279bd0037a25bc0c058596ae6f4b0ab86f787
tiennhot23 <45113732+tiennhot23@users.noreply.github.com>
2021-06-22 20:57:09 +0700

    add splash screen

--- app/src/main/java/com/e/mycomicreader/views/MainActivity.java
+++ app/src/main/java/com/e/mycomicreader/views/MainActivity.java
@@ -1,5 +1,7 @@
 package com.e.mycomicreader.views;
 
+import android.content.Intent;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.e.mycomicreader.R;
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startSplashScreen();
+
+    }
+
+    private void startSplashScreen() {
+        Intent intent = new Intent(this, SplashScreen.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/e/mycomicreader/views/SplashScreen.java
+++ app/src/main/java/com/e/mycomicreader/views/SplashScreen.java
@@ -1,14 +1,34 @@
 package com.e.mycomicreader.views;
 
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.e.mycomicreader.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Animation animSplashScreen;
+    ImageView imgSplashScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        animSplashScreen = AnimationUtils.loadAnimation(this, R.anim.anim_splashscreen);
+        imgSplashScreen = findViewById(R.id.img_splash_screen);
+        animSplashScreen.setDuration(2000);
+        imgSplashScreen.setAnimation(animSplashScreen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 5000);
     }
 }
